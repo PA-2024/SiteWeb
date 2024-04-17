e947b0293fd8f74197dc9e259f2153c504fd4c50
CapDRAKE <72074285+CapDRAKE@users.noreply.github.com>
2024-04-17 23:23:14 +0200

    Create performance-test.js

---
+++ tests/performance-test.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 100 }, // Va simuler 100 utilisateurs pendant 1 minute
+    ]
+};
+
+export default function () {
+    http.get('https://pa.wstr.fr');
+    sleep(1);
+}
