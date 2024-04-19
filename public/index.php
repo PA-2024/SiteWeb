5ee46498d050c5965fde2caea144c9befcb85252
Capdrake <bastienleuwers2@gmail.com>
2024-04-19 12:48:45 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,8 @@
 <?php
 // Auteur : Capdrake (Bastien LEUWERS)
 
-header("HTTP/1.0 404 Not Found");
-include 'error-404.php';
-exit();
-
 require_once '../vendor/autoload.php';
 
-if (class_exists("GeSign\SessionManager")) {
-    echo "La classe SessionManager existe.";
-} else {
-    echo "La classe SessionManager n'existe pas.";
-}
 use GeSign\SessionManager;
 
 $sessionManager = new SessionManager();
