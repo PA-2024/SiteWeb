fdd7d4f5e7d039875b7a107e4c228684188316f6
Capdrake <bastienleuwers2@gmail.com>
2024-04-19 12:43:19 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 // Auteur : Capdrake (Bastien LEUWERS)
 
+header("HTTP/1.0 404 Not Found");
+include 'error-404.php';
+exit();
+
 require_once '../vendor/autoload.php';
 
 if (class_exists("GeSign\SessionManager")) {
