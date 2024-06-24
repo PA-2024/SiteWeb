8dc618d4b77b146a1d0f139c2f222d10fe2098b1
Capdrake <bastienleuwers2@gmail.com>
2024-06-24 12:12:55 +0200

    Update menu_prof.php

--- public/menu/menu_prof.php
+++ public/menu/menu_prof.php
@@ -7,19 +7,19 @@
 				<li class="submenu">
 					<a href="#"><span class="menu-side"><img src="assets/img/icons/menu-icon-03.svg" alt=""></span> <span>Membres </span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a href="members.php">Liste des membres</a></li>
-						<li><a href="add-members.php">Ajouter un membre</a></li>
-						<li><a href="edit-members.php">Editer un membre</a></li>
+						<li><a href="members.php">Appel</a></li>
+						<li><a href="add-members.php">Faire l'appel</a></li>
+						<li><a href="edit-members.php">Voir l'appel</a></li>
 					</ul>
 				</li>
 				<li class="submenu">
 					<a href="#"><span class="menu-side"><img src="assets/img/icons/menu-icon-06.svg" alt=""></span> <span>Gestion des écoles </span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
 						<li class="submenu">
-                            <a href="#"></i> <span> Ecoles</span> <span class="menu-arrow"></span></a>
+                            <a href="#"></i> <span>QCM</span> <span class="menu-arrow"></span></a>
                             <ul style="display: none;">
-                                <li><a href="schools_list.php">Liste des écoles</a></li>
-								<li><a href="add_school.php">Ajouter un école</a></li>
+                                <li><a href="schools_list.php">Ajouter un QCM</a></li>
+								<li><a href="add_school.php">Voir les résultats</a></li>
                             </ul>
                         </li>
 					</ul>
