647872735de304b52e93be659b93284c099a0a8d
Capdrake <bastienleuwers2@gmail.com>
2024-06-26 18:24:19 +0200

    Update SessionManager.php
    Optimize

--- src/SessionManager.php
+++ src/SessionManager.php
@@ -5,49 +5,56 @@ namespace GeSign;
 
 class SessionManager
 {
-    public function checkUserLoggedIn()
+    public function __construct()
     {
         session_start();
+    }
 
-        if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
-            if (isset($_COOKIE['user_logged_in']) && $_COOKIE['user_logged_in'] === 'true') {
-                $_SESSION['user_logged_in'] = true;
-                $_SESSION['user_id'] = $_COOKIE['user_id'];
-                $_SESSION['user_name'] = $_COOKIE['user_name'];
-                $_SESSION['user_role'] = $_COOKIE['user_role'];
-                $_SESSION['token'] = $_COOKIE['token'];
-                $_SESSION['school'] = $_COOKIE['school'];
-                $_SESSION['schoolId'] = $_COOKIE['schoolId'];
-                $this->redirectToDashboard2();
-            } else {
-                $this->redirectToLogin();
-            }
+    public function checkUserLoggedIn()
+    {
+        if (!$this->isUserLoggedIn()) {
+            $this->attemptLoginFromCookies();
+        }
+
+        if ($this->isUserLoggedIn()) {
+            $this->redirectToDashboard();
         } else {
-            $this->redirectToDashboard2();
+            $this->redirectToLogin();
+        }
+    }
+
+    private function isUserLoggedIn()
+    {
+        return isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] === true;
+    }
+
+    private function attemptLoginFromCookies()
+    {
+        if (isset($_COOKIE['user_logged_in']) && $_COOKIE['user_logged_in'] === 'true') {
+            $_SESSION['user_logged_in'] = true;
+            $_SESSION['user_id'] = $_COOKIE['user_id'];
+            $_SESSION['user_name'] = $_COOKIE['user_name'];
+            $_SESSION['user_role'] = $_COOKIE['user_role'];
+            $_SESSION['token'] = $_COOKIE['token'];
+            $_SESSION['school'] = $_COOKIE['school'];
+            $_SESSION['schoolId'] = $_COOKIE['schoolId'];
         }
     }
 
     protected function redirectToLogin()
     {
-        header('Location: views/auth/login.php');
-        exit;
-    }
-
-    protected function redirectToLogin2()
-    {
-        header('Location: ../views/auth/login.php');
+        header('Location: /views/auth/login.php');
         exit;
     }
 
     protected function redirectToError()
     {
-        header('Location: views/misc/error-404.php');
+        header('Location: /views/misc/error-404.php');
         exit;
     }
 
     public function loginUser($userId, $userName, $userRole, $token, $school, $schoolId, $remember = false)
     {
-        session_start();
         $_SESSION['user_logged_in'] = true;
         $_SESSION['user_id'] = $userId;
         $_SESSION['user_name'] = $userName;
@@ -57,16 +64,22 @@ class SessionManager
         $_SESSION['schoolId'] = $schoolId;
 
         if ($remember) {
-            setcookie('user_logged_in', 'true', time() + (86400 * 30), "/"); // 30 jours
-            setcookie('user_id', $userId, time() + (86400 * 30), "/");
-            setcookie('user_name', $userName, time() + (86400 * 30), "/");
-            setcookie('user_role', $userRole, time() + (86400 * 30), "/");
-            setcookie('token', $token, time() + (86400 * 30), "/");
-            setcookie('school', $school, time() + (86400 * 30), "/");
-            setcookie('schoolId', $school, time() + (86400 * 30), "/");
+            $this->setRememberMeCookies($userId, $userName, $userRole, $token, $school, $schoolId);
         }
 
         $this->redirectToDashboard();
+    }
+
+    private function setRememberMeCookies($userId, $userName, $userRole, $token, $school, $schoolId)
+    {
+        $cookieExpiration = time() + (86400 * 30); // 30 jours
+        setcookie('user_logged_in', 'true', $cookieExpiration, "/");
+        setcookie('user_id', $userId, $cookieExpiration, "/");
+        setcookie('user_name', $userName, $cookieExpiration, "/");
+        setcookie('user_role', $userRole, $cookieExpiration, "/");
+        setcookie('token', $token, $cookieExpiration, "/");
+        setcookie('school', $school, $cookieExpiration, "/");
+        setcookie('schoolId', $schoolId, $cookieExpiration, "/");
     }
 
     protected function redirectToDashboard()
@@ -75,80 +88,42 @@ class SessionManager
             $this->redirectToError();
         }
 
-        switch ($_SESSION['user_role']) {
-            case 'Admin':
-                header('Location: ../dashboard/admin_dashboard.php');
-                break;
-            case 'Gestion Ecole':
-                header('Location: ../dashboard/director_dashboard.php');
-                break;
-            case 'Professeur':
-                header('Location: ../dashboard/professor_dashboard.php');
-                break;
-            case 'Eleve':
-                header('Location: ../dashboard/student_dashboard.php');
-                break;
-            default:
-                $this->redirectToError();
-        }
-        exit;
-    }
+        $role = $_SESSION['user_role'];
+        $dashboardMap = [
+            'Admin' => '/views/dashboard/admin_dashboard.php',
+            'Gestion Ecole' => '/views/dashboard/director_dashboard.php',
+            'Professeur' => '/views/dashboard/professor_dashboard.php',
+            'Eleve' => '/views/dashboard/student_dashboard.php'
+        ];
 
-    protected function redirectToDashboard2()
-    {
-        if (!isset($_SESSION['user_role'])) {
+        if (isset($dashboardMap[$role])) {
+            header('Location: ' . $dashboardMap[$role]);
+            exit;
+        } else {
             $this->redirectToError();
         }
-
-        switch ($_SESSION['user_role']) {
-            case 'Admin':
-                header('Location: views/dashboard/admin_dashboard.php');
-                break;
-            case 'Gestion Ecole':
-                header('Location: views/dashboard/director_dashboard.php');
-                break;
-            case 'Professeur':
-                header('Location: views/dashboard/professor_dashboard.php');
-                break;
-            case 'Eleve':
-                header('Location: views/dashboard/student_dashboard.php');
-                break;
-            default:
-                $this->redirectToError();
-        }
-        exit;
     }
 
     public function logoutUser()
     {
-        session_start();
         session_destroy();
+        $this->clearCookies();
+        $this->redirectToLogin();
+    }
 
-        setcookie('user_logged_in', '', time() - 3600, "/");
-        setcookie('user_id', '', time() - 3600, "/");
-        setcookie('user_name', '', time() - 3600, "/");
-        setcookie('user_role', '', time() - 3600, "/");
-        setcookie('token', '', time() - 3600, "/");
-        setcookie('school', '', time() - 3600, "/");
-        setcookie('schoolId', '', time() - 3600, "/");
-
-        $this->redirectToLogin2();
+    private function clearCookies()
+    {
+        $cookies = ['user_logged_in', 'user_id', 'user_name', 'user_role', 'token', 'school', 'schoolId'];
+        foreach ($cookies as $cookie) {
+            setcookie($cookie, '', time() - 3600, "/");
+        }
     }
 
     public function restrictAccessToLoginUsers()
     {
-        session_start();
-
-        if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
-            if (isset($_COOKIE['user_logged_in']) && $_COOKIE['user_logged_in'] === 'true') {
-                $_SESSION['user_logged_in'] = true;
-                $_SESSION['user_id'] = $_COOKIE['user_id'];
-                $_SESSION['user_name'] = $_COOKIE['user_name'];
-                $_SESSION['user_role'] = $_COOKIE['user_role'];
-                $_SESSION['token'] = $_COOKIE['token'];
-                $_SESSION['school'] = $_COOKIE['school'];
-                $_SESSION['schoolId'] = $_COOKIE['schoolId'];
-            } else {
+        if (!$this->isUserLoggedIn()) {
+            $this->attemptLoginFromCookies();
+            if (!$this->isUserLoggedIn()) {
                 $this->redirectToError();
             }
         }
@@ -157,8 +132,7 @@ class SessionManager
     public function checkUserRole($requiredRole)
     {
         if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== $requiredRole) {
-            header('Location: ../misc/error-404.php');
-            exit;
+            $this->redirectToError();
         }
     }
 }
