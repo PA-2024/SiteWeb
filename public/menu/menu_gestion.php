3b6f8b2e8a8e928a5e45c325db94c1bf5292949c
CapDRAKE <72074285+CapDRAKE@users.noreply.github.com>
2024-07-13 12:28:23 +0200

    Update menu_gestion.php

--- public/menu/menu_gestion.php
+++ public/menu/menu_gestion.php
@@ -11,33 +11,28 @@
                     </ul>
                 </li>
                 <li class="submenu">
-                    <a href="#"><span class="menu-side"><img src="../../assets/img/icons/menu-icon-03.svg" alt=""></span> <span>Classe </span> <span class="menu-arrow"></span></a>
+                    <a href="#"><span class="menu-side"><img src="../../assets/img/icons/menu-icon-03.svg" alt=""></span> <span>Étudiants </span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="../lists/student_list.php">Liste des étudiants</a></li>
+                        <li><a href="../forms/add_student.php">Ajouter un étudiant</a></li>
+                        <li><a href="../lists/justif_list.php">Justification des absences</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="#"><span class="menu-side"><img src="../../assets/img/icons/menu-icon-06.svg" alt=""></span> <span>Classes </span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
                         <li><a href="../lists/sectors_list.php">Liste des classes</a></li>
-                        <li><a href="../lists/view_students.php">Liste des étudiants d'une classe</a></li>
+                        <li><a href="../lists/view_students.php">Liste des étudiants par classe</a></li>
                         <li><a href="../forms/add_sectors.php">Ajouter une classe</a></li>
                     </ul>
                 </li>
                 <li class="submenu">
-                    <a href="#"><span class="menu-side"><img src="../../assets/img/icons/menu-icon-03.svg" alt=""></span> <span>Etudiants </span> <span class="menu-arrow"></span></a>
+                    <a href="#"><span class="menu-side"><img src="../../assets/img/icons/menu-icon-06.svg" alt=""></span> <span>Cours </span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="../lists/student_list.php">Liste des étudiants</a></li>
-                        <li><a href="../lists/justif_list.php">Justification des absences</a></li>
-                        <li><a href="../forms/add_student.php">Ajouter des étudiants</a></li>
-                    </ul>
-                </li>
-                <li class="submenu">
-                    <a href="#"><span class="menu-side"><img src="../../assets/img/icons/menu-icon-06.svg" alt=""></span> <span>Gestion des cours </span> <span class="menu-arrow"></span></a>
-                    <ul style="display: none;">
+                        <li><a href="../lists/subjects_list.php">Liste des cours</a></li>
+                        <li><a href="../forms/add_subjects.php">Ajouter un cours</a></li>
                         <li class="submenu">
-                            <a href="#"><span> Cours</span> <span class="menu-arrow"></span></a>
-                            <ul style="display: none;">
-                                <li><a href="../lists/subjects_list.php">Liste des cours</a></li>
-                                <li><a href="../forms/add_subjects.php">Ajouter un cours</a></li>
-                            </ul>
-                        </li>
-                        <li class="submenu">
-                            <a href="#"><span>Etudiants</span> <span class="menu-arrow"></span></a>
+                            <a href="#"><span>Gestion des cours</span> <span class="menu-arrow"></span></a>
                             <ul style="display: none;">
                                 <li><a href="../forms/add_student_subject.php">Ajouter un étudiant à un cours</a></li>
                                 <li><a href="../forms/add_sectors_subject.php">Ajouter une classe à un cours</a></li>
@@ -45,7 +40,7 @@
                             </ul>
                         </li>
                         <li class="submenu">
-                            <a href="#"><span> Heures de Cours</span> <span class="menu-arrow"></span></a>
+                            <a href="#"><span>Heures de cours</span> <span class="menu-arrow"></span></a>
                             <ul style="display: none;">
                                 <li><a href="../lists/list_subject_hours.php">Liste des heures de cours</a></li>
                                 <li><a href="../forms/add_subject_hour.php">Ajouter une heure de cours</a></li>
@@ -58,12 +53,13 @@
                     <a href="#"><i class="fa fa-columns"></i> <span>Bâtiments</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
                         <li><a href="../lists/buildings_list.php">Liste des bâtiments</a></li>
-                        <li><a href="../forms/add_building.php">Ajouter des bâtiments</a></li>
+                        <li><a href="../forms/add_building.php">Ajouter un bâtiment</a></li>
                     </ul>
                 </li>
-                <div class="logout-btn">
-                    <a href="../../script/logout.php"><span class="menu-side"><img src="../../assets/img/icons/logout.svg" alt=""></span> <span>Me déconnecter</span></a>
-                </div>
+                <li class="menu-title">Autres</li>
+                <li>
+                    <a href="../../script/logout.php"><span class="menu-side"><img src="../../assets/img/icons/logout.svg" alt=""></span> <span>Déconnexion</span></a>
+                </li>
             </ul>
         </div>
     </div>
